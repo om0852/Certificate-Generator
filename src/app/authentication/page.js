1a92406e7ebaff69bad376b6ff6dc0e66da153af
RushabhRatnaparkhi <120812724+RushabhRatnaparkhi@users.noreply.github.com>
2024-05-17 12:27:36 +0530

    userlogin changes

---
+++ src/app/authentication/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import AuthComponent from "../component/AuthComponent";
+import React from 'react'
+
+const Authentication = () => {
+    return (
+        <div>
+            <AuthComponent />
+        </div>
+    )
+}
+
+export default Authentication
